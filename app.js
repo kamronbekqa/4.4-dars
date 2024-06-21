513627c7a1edeaddd29116402387e252c4ebd6d2
kamronbekqa <168660944+kamronbekqa@users.noreply.github.com>
2024-06-21 11:11:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let son = Math.floor(Math.random() * 100);
+
+if (son % 3 === 0 && son % 5 === 0) {
+  alert(`${son} noni fuzzbuzz`);
+} else if (son % 5 === 0) {
+  alert(`${son} soni buzz`);
+} else if (son % 3 === 0) {
+  alert(`${son} soni fizz`);
+}else{
+alert`    bu son 3ga ham 4ga ham bolinmaydi
+`;}
